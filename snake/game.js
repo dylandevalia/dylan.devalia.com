257822c60b18fb71447bd033368fdca385620954
Dylan Devalia <dylan@devalia.com>
2017-02-11 18:13:06 +0000

    Initial commit

---
+++ snake/game.js
@@ -0,0 +1,94 @@
+var state;
+
+var scl = 50;
+// var gridSize = Math.min(Math.floor(innerWidth / scl), Math.floor(innerHeight / scl));
+var w = Math.floor(innerWidth / scl);
+var h = Math.floor(innerHeight / scl);
+
+var snake;
+var food;
+var counter;
+
+/**
+ *  Creates canvas and all object on page load
+ */
+function setup() {
+	/* Create objects */
+	createCanvas(w * scl, h * scl);
+	// frameRate(60);
+	init();
+}
+
+/**
+ *  Initialises game board
+ */
+function init() {
+	state = "PLAY";
+	counter = 15;
+	snake = new Snake();
+	food = new Pellet();
+}
+
+/**
+ *  Updates and draws all objects
+ */
+function draw() {
+	background(66);
+	strokeWeight(0);
+	
+	if (state == "PLAY") {
+		state_play();
+	} else if (state == "DEATH") {
+		state_dead();
+	} else if (state == "WIN") {
+		state_won();
+	}
+}
+
+/**
+ *  Runs the game
+ */
+function state_play() {
+	if (frameCount % counter == 0) {
+		/* Update and show snake */
+		snake.update();
+		snake.death();
+		snake.eat(food);
+	}
+	
+	/* Show objects */
+	food.show();
+	snake.show();
+}
+
+/**
+ *  When the player dies
+ */
+function state_dead() {
+	print("Dead");
+	background(255, 0, 0);
+	init();
+}
+
+/**
+ *  When the game is won
+ */
+function state_won() {
+	print("Won");
+}
+
+/**
+ *  Event listener for all key presses
+ *  Uses arrow keys and (WASD) to control
+ */
+function keyPressed() {
+	if (keyCode === UP_ARROW || keyCode === 87) {
+		snake.dir(0, -1);
+	} else if (keyCode === DOWN_ARROW || keyCode === 83) {
+		snake.dir(0, 1);
+	} else if (keyCode === RIGHT_ARROW || keyCode === 68) {
+		snake.dir(1, 0);
+	} else if (keyCode === LEFT_ARROW || keyCode === 65) {
+		snake.dir(-1, 0);
+	}
+}
---
+++ snake/pellet.js
@@ -0,0 +1,62 @@
+function Pellet() {
+	this.pos = createVector(floor(random(w)), floor(random(h))).mult(scl);
+	this.col = floor(random(7));
+	
+	// this.getFreeTile = function(snake) {
+	// 	var tmp;
+	// 	var d = -1;
+	// 	while (d < 1) {
+	// 		tmp = createVector(floor(random(gridSize)), floor(random(gridSize))).mult(scl);
+	// 		d = dist(snake.pos.x, snake.pos.y, tmp.x, tmp.y);
+	//
+	// 		for (var i = 0; d > 1 && i < snake.tail.length; i++) {
+	// 			d = dist(snake.tail[i].x, snake.tail[i].y, tmp.x, tmp.y)
+	// 		}
+	// 	}
+	//
+	// 	return tmp;
+	// }
+	
+	/**
+	 *  Shows the pellet object
+	 */
+	this.show = function() {
+		switch (this.col) {
+			case 0:
+				fill(179, 229, 252);
+				break;
+			case 1:
+				fill(129, 212, 250);
+				break;
+			case 2:
+				fill(79, 159, 247);
+				break;
+			case 3:
+				fill(41, 182, 246);
+				break;
+			case 4:
+				fill(3, 169, 244);
+				break;
+			case 5:
+				fill(3, 155, 229);
+				break;
+			case 6:
+				fill(2, 136, 209);
+				break;
+		}
+		rect(this.pos.x, this.pos.y, scl, scl)
+	}
+	
+	/**
+	 *  Seclects a new position if it has been eaten
+	 *  @param snake
+	 */
+	this.eaten = function(snake) {
+		/* Move to new position */
+		this.pos.x = floor(random(w)) * scl;
+		this.pos.y = floor(random(h)) * scl;
+		
+		/* Change colour */
+		this.col = floor(random(7));
+	}
+}
