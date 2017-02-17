7c6935d3124a6e7bbf4f625412c3163861ef3885
Dylan Devalia <dylan@devalia.com>
2017-02-17 19:56:24 +0000

    Stars now have a random size

--- starfeild/star.js
+++ starfeild/star.js
@@ -2,10 +2,12 @@ function Star() {
 	this.pos = createVector(
 		random(-width / 2, width / 2),      // x
 		random(-height / 2, height / 2),    // y
-		random(1, width)                // z
+		random(1, width)                    // z
 	);
 	
 	this.pz = this.pos.z;
+	
+	this.maxSize = random(2, 15);
 	
 	this.update = function() {
 		this.pos.z -= speed;
@@ -21,7 +23,7 @@ function Star() {
 	this.show = function() {
 		var sx = map(this.pos.x / this.pos.z, 0, 1, 0, width);
 		var sy = map(this.pos.y / this.pos.z, 0, 1, 0, height);
-		var r = map(this.pos.z, 0, width, 5, 0);
+		var r = map(this.pos.z, 0, width, this.maxSize, 0);
 		
 		var px = map(this.pos.x / this.pz, 0, 1, 0, width);
 		var py = map(this.pos.y / this.pz, 0, 1, 0, height);
