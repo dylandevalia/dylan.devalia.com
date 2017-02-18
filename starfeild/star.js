006f91a052e26d8f851e49b778a94d1a0f44130b
Dylan Devalia <dylan@devalia.com>
2017-02-18 02:01:02 +0000

    Replaced object functions with prototypes

--- starfeild/star.js
+++ starfeild/star.js
@@ -4,36 +4,35 @@ function Star() {
 		random(-height / 2, height / 2),    // y
 		random(1, width)                    // z
 	);
-	
 	this.pz = this.pos.z;
 	
 	this.maxSize = random(2, 15);
-	
-	this.update = function() {
-		this.pos.z -= speed;
-		if (this.pos.z < 1) {
-			this.pos.x = random(-width / 2, width / 2);
-			this.pos.y = random(-height / 2, height / 2);
-			this.pos.z = random(1, width);
-			
-			this.pz = this.pos.z;
-		}
-	}
-	
-	this.show = function() {
-		var sx = map(this.pos.x / this.pos.z, 0, 1, 0, width);
-		var sy = map(this.pos.y / this.pos.z, 0, 1, 0, height);
-		var r = map(this.pos.z, 0, width, this.maxSize, 0);
-		
-		var px = map(this.pos.x / this.pz, 0, 1, 0, width);
-		var py = map(this.pos.y / this.pz, 0, 1, 0, height);
-		
-		
-		stroke(238, 200);
-		fill(238, 200);
-		ellipse(sx, sy, r, r);
-		line(px, py, sx, sy);
+}
+
+Star.prototype.update = function() {
+	this.pos.z -= speed;
+	if (this.pos.z < 1) {
+		this.pos.x = random(-width / 2, width / 2);
+		this.pos.y = random(-height / 2, height / 2);
+		this.pos.z = random(1, width);
 		
 		this.pz = this.pos.z;
 	}
-}
+};
+
+Star.prototype.show = function() {
+	var sx = map(this.pos.x / this.pos.z, 0, 1, 0, width);
+	var sy = map(this.pos.y / this.pos.z, 0, 1, 0, height);
+	var r = map(this.pos.z, 0, width, this.maxSize, 0);
+	
+	var px = map(this.pos.x / this.pz, 0, 1, 0, width);
+	var py = map(this.pos.y / this.pz, 0, 1, 0, height);
+	
+	
+	stroke(238, 200);
+	fill(238, 200);
+	ellipse(sx, sy, r, r);
+	line(px, py, sx, sy);
+	
+	this.pz = this.pos.z;
+};
