b155d867dfa00d3e590bebf6928a9f7985c140d4
Dylan Devalia <dylan@devalia.com>
2017-02-24 17:04:28 +0000

    Organised index.html files and started playing around with sliding sidebars

--- starfield/main.js
+++ starfield/main.js
@@ -1,5 +1,8 @@
 var stars = [];
 var speed = 10;
+var decelerate = false;
+var targetSpeed = speed;
+var tmpSpeed;
 
 function setup() {
 	createCanvas(innerWidth, innerHeight);
@@ -10,22 +13,29 @@ function setup() {
 
 function draw() {
 	background(66);
-	//noStroke();
-	
 	translate(width / 2, height / 2);
 	
 	for (var i = 0; i < stars.length; i++) {
 		stars[i].update();
 		stars[i].show();
 	}
+	
+	if (decelerate && frameCount % 10 == 0) {
+		speed -= 10;
+		if (speed == 10) {
+			speed = 10;
+			decelerate = false;
+		}
+	}
 }
 
 function mousePressed() {
 	if (mouseButton == LEFT) {
 		speed += 10;
-		if (speed > 100) {
-			speed = 10;
-		}
+	} else if (mouseButton == RIGHT) {
+		decelerate = true;
+		targetSpeed -= floor(speed / 3);
+		tmpSpeed = speed;
 	}
 }
 
