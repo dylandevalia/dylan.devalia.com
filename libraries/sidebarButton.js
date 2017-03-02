516cacf9c790b923084ac6e642c7093e06691a31
Dylan Devalia <dylan@devalia.com>
2017-03-02 12:47:58 +0000

    Changed sidebar to shift rather than change size

--- libraries/sidebarButton.js
+++ libraries/sidebarButton.js
@@ -31,12 +31,12 @@ function toggleNav() {
 
 /* Set the width of the side navigation to 250px */
 function openNav() {
-	document.getElementById("sidenav").style.width = "250px";
+	document.getElementById("sidenav").style.left = "0px";
 	isNavOpen = true;
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
-	document.getElementById("sidenav").style.width = "0";
+	document.getElementById("sidenav").style.left = "-250px";
 	isNavOpen = false;
 }
